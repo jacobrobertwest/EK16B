518b81bada607959a6ee6c377e66606977e364f5
Jacob R West <38504417+jacobrobertwest@users.noreply.github.com>
2023-12-17 15:43:21 -0700

    v0.0.2

--- code/main.py
+++ code/main.py
@@ -7,6 +7,19 @@
 import pygame
 from sys import exit
 
+# create functions
+def display_score(end_time=None):
+    if end_time is None:
+        curtime = pygame.time.get_ticks() - start_time
+    else:
+        curtime = end_time - start_time
+    current_time = curtime // 100
+    score_surface = test_font.render(f'Score: {current_time}',False,(64,64,64))
+    score_rect = score_surface.get_rect(midtop = (240,25))
+    pygame.draw.rect(screen,'#C4A968',score_rect)
+    screen.blit(score_surface,score_rect)
+
+
 # initializing pygame & setting up the screen
 pygame.init()
 screen_width = 480
@@ -17,6 +30,7 @@ pygame.display.set_icon(pygame.image.load('graphics/logo_ek16b.png'))
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/AncientModernTales.ttf',30)
 game_active = True
+start_time = 0
 
 # creating background & npc surfaces & setting initial variables
 bg_surface = pygame.image.load('graphics/bg_test.png').convert_alpha()
@@ -28,6 +42,14 @@ monk_rect = monk_surface.get_rect(topleft=(400,200))
 # creating player surface
 player_surface = pygame.image.load('graphics/spr_link.png').convert_alpha()
 player_rect = player_surface.get_rect(topleft=(145,200))
+
+# creating scaled player
+player_scaled = pygame.transform.scale(player_surface,(64,64))
+player_scaled_rect = player_scaled.get_rect(center=(240,160))
+
+# creating press_start_text
+start_text_surface = test_font.render('Ellie Kemper: 16-Bit Edition', False, '#DE4E2A')
+start_text_rect = start_text_surface.get_rect(center=(240, 280))
 
 # the game loop
 while True:
@@ -46,14 +68,12 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 monk_rect.left = 400
+                start_time = pygame.time.get_ticks()
 
 
     if game_active:
         # draw all our elements & update everything
         screen.blit(bg_surface,(0,0))
-        pygame.draw.rect(screen,'#C4A968',text_rect)
-        pygame.draw.rect(screen,'#C4A968',text_rect,10)
-        screen.blit(text_surface,text_rect)
 
         monk_rect.left -= 2 # decrement monkey position by 2
         if monk_rect.left == -16:
@@ -74,11 +94,16 @@ while True:
         if player_rect.right >= 480: player_rect.right = 480
 
         screen.blit(player_surface,player_rect)
-
+    
+        display_score()
         if monk_rect.colliderect(player_rect):
             game_active = False
+            final_time = pygame.time.get_ticks()
     else:
         screen.fill('#C4A968')
+        screen.blit(player_scaled,player_scaled_rect)
+        display_score(final_time)
+        screen.blit(start_text_surface, start_text_rect)
 
 
 
