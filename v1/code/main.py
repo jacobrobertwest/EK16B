e99fc5a3ea3b0808abadfcebbb9985a8b7abd29a
Jacob R West <38504417+jacobrobertwest@users.noreply.github.com>
2023-12-22 20:17:42 -0700

    v1.3

--- v1/code/main.py
+++ v1/code/main.py
@@ -7,13 +7,13 @@ class Game:
 		  
 		# general setup
 		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH),pygame.DOUBLEBUF)
 		pygame.display.set_caption('ELLIE KEMPER: 16-Bit Edition (v1)')
 		pygame.display.set_icon(pygame.image.load('graphics/logo_ek16b.png'))
+
 		self.clock = pygame.time.Clock()
-		
 		self.level = Level()
-	
+	 
 	# this is the ultimate run "event loop" that consists of the actual game
 	def run(self):
 		while True:
--- v1/code/tile.py
+++ v1/code/tile.py
@@ -5,8 +5,13 @@ from settings import *
 # this Tile class inherits everything from Sprite class
 # in essence, a Tile is a Sprite
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(self,pos,groups,sprite_type,surface = pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(groups) # we gotta use this to initialize our base/parent class!
-        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos) #creating a rectangle based on the image attribute, putting the position at the top left
-        
+        self.sprite_type = sprite_type
+        self.image = surface
+        if self.sprite_type == 'object':
+            # do an offset
+            self.rect = self.image.get_rect(topleft = (pos[0],pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft = pos) #creating a rectangle based on the image attribute, putting the position at the top left
+        self.hitbox = self.rect.inflate(0,-10)
